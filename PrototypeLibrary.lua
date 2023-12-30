e5420bba7aecd766e30ac541cd3a919885ef716b
Λstralic <140242928+Astralic62@users.noreply.github.com>
2023-12-30 15:36:10 +0200

    Update PrototypeLibrary.lua

--- PrototypeLibrary.lua
+++ PrototypeLibrary.lua
@@ -18,7 +18,7 @@ SG.Parent = gethui()
 
 local Back = Instance.new("Frame")
 Back.Name = "Back"
-Back.BackgroundColor3 = Color3.fromHex("000000")
+Back.BackgroundColor3 = Color3.fromHex("121318")
 Back.Size = UDim2.new(0, 450, 0, 300)
 Back.Draggable = true
 Back.BorderSizePixel = 0
@@ -31,7 +31,7 @@ local X = Instance.new("TextButton")
 X.Name = "X"
 X.BackgroundTransparency = 1
 X.Font = 9
-X.TextColor3 = Color3.fromHex("ffffff")
+X.TextColor3 = Color3.fromHex("7a7a84")
 X.Size = UDim2.new(0, 30, 0, 30)
 X.Text = "X"
 X.TextScaled = true
@@ -45,7 +45,7 @@ local TitleUI = Instance.new("TextLabel")
 TitleUI.Name = "TitleUI"
 TitleUI.Size = UDim2.new(0, 300, 0, 30)
 TitleUI.Text = Title
-TitleUI.TextColor3 = Color3.fromHex("ffffff")
+TitleUI.TextColor3 = Color3.fromHex("7a7a84")
 TitleUI.Font = 9
 TitleUI.Position = UDim2.new(0, 10, 0, 10)
 TitleUI.TextScaled = true
@@ -58,7 +58,7 @@ ToggleUI.Name = "ToggleUI"
 ToggleUI.Font = 9
 ToggleUI.Size = UDim2.new(0, 32, 0, 32)
 ToggleUI.TextScaled = true
-ToggleUI.TextColor3 = Color3.fromHex("ffffff")
+ToggleUI.TextColor3 = Color3.fromHex("7a7a84")
 ToggleUI.Position = UDim2.new(1, -224, 0, 4)
 ToggleUI.Text = "P"
 ToggleUI.BackgroundTransparency = 0.4
@@ -71,7 +71,7 @@ end)
 
 local TabsOpenContainer = Instance.new("ScrollingFrame")
 TabsOpenContainer.ScrollingDirection = 2
-TabsOpenContainer.BackgroundColor3 = Color3.fromHex("1f1f1f")
+TabsOpenContainer.BackgroundColor3 = Color3.fromHex("17181d")
 TabsOpenContainer.CanvasSize = UDim2.new(0, 0, 0, 0)
 TabsOpenContainer.AutomaticCanvasSize = 2
 TabsOpenContainer.Name = "TabsOpenContainer"
@@ -89,7 +89,7 @@ TabsContainer.Size = UDim2.new(0, 315, 0, 245)
 TabsContainer.BorderSizePixel = 0
 TabsContainer.Name = "TabsContainer"
 TabsContainer.Position = UDim2.new(0, 125, 0, 45)
-TabsContainer.BackgroundColor3 = Color3.fromHex("1f1f1f")
+TabsContainer.BackgroundColor3 = Color3.fromHex("17181d")
 TabsContainer.Parent = Back
 
 local MakeATab = {}
@@ -98,11 +98,11 @@ function MakeATab:MakeTab(Title, Visible)
 local OpenTab = Instance.new("TextButton")
 OpenTab.BorderSizePixel = 0
 OpenTab.Name = "OpenTab"
-OpenTab.TextColor3 = Color3.fromHex("ffffff")
+OpenTab.TextColor3 = Color3.fromHex("7a7a84")
 OpenTab.Text = Title
 OpenTab.Size = UDim2.new(1, 0, 0, 30)
 OpenTab.TextScaled = true
-OpenTab.BackgroundColor3 = Color3.fromHex("2e2e2e")
+OpenTab.BackgroundColor3 = Color3.fromHex("1d1d27")
 OpenTab.Font = 9
 OpenTab.Parent = TabsOpenContainer
 
@@ -138,7 +138,7 @@ Info.BackgroundTransparency = 1
 Info.Text = Title
 Info.TextXAlignment = 0
 Info.Name = "Info"
-Info.TextColor3 = Color3.fromHex("ffffff")
+Info.TextColor3 = Color3.fromHex("7a7a84")
 Info.Parent = Tab
 end
 
@@ -146,12 +146,12 @@ function TabElements:Button(Title, Cally)
 local Button = Instance.new("TextButton")
 Button.TextXAlignment = 0
 Button.Name = "Button"
-Button.TextColor3 = Color3.fromHex("ffffff")
+Button.TextColor3 = Color3.fromHex("7a7a84")
 Button.Text = Title
 Button.Font = 9
 Button.Size = UDim2.new(1, 0, 0, 30)
 Button.TextScaled = true
-Button.BackgroundColor3 = Color3.fromHex("2e2e2e")
+Button.BackgroundColor3 = Color3.fromHex("1d1d27")
 Button.BorderSizePixel = 0
 Button.Parent = Tab
 Button.MouseButton1Click:Connect(function()
@@ -163,9 +163,9 @@ function TabElements:Toggle(Title, Cally)
 local Tog = Instance.new("TextLabel")
 Tog.Size = UDim2.new(1, -60, 0, 30)
 Tog.BorderSizePixel = 0
-Tog.BackgroundColor3 = Color3.fromHex("2e2e2e")
+Tog.BackgroundColor3 = Color3.fromHex("1d1d27")
 Tog.TextXAlignment = 0
-Tog.TextColor3 = Color3.fromHex("ffffff")
+Tog.TextColor3 = Color3.fromHex("7a7a84")
 Tog.Text = Title
 Tog.Font = 9
 Tog.TextScaled = true
